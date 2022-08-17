cdf3519b59d34006293e088c53c83898d2edc48c
onear <mifen2017@qq.com>
2022-08-17 19:48:03 +0800

    设置的界面布局

--- app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
+++ app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
@@ -3,16 +3,17 @@ package com.oldphonetoolbox.onear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SettingActivity extends AppCompatActivity {
     //数据源，tittleArray为标题，descriptionArray为描述
-    private static final int[] tittleArray = {R.string.settings_color_tittle,};
-    private static final int[] descriptionArray = {R.string.settings_color_desc};
+    private final String[] titleArray = {getResources().getString(R.string.settings_color_title),getResources().getString(R.string.settings_st2_desc)};
+    private final String[] descriptionArray = {getResources().getString(R.string.settings_color_desc),getResources().getString(R.string.settings_st2_desc)};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +22,16 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void showListview() {
-        List<Map<String,Object>> list = new ArrayList<>();
-        for (int i = 0; i < tittleArray.length; i++) {
-            
+        List<HashMap<String, String>> list = new ArrayList<>();
+        for (int i = 0; i < titleArray.length; i++) {
+            HashMap <String,String> item = new HashMap<>();
+            item.put("tittle",titleArray[i]);
+            item.put("desc",descriptionArray[i]);
+            list.add(item);
         }
-        //SimpleAdapter simpleAdapter = new SimpleAdapter(this,list);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,list,R.layout.simple_list_item,new String[]{"tittle","desc"},new int[]{R.id.setting_item_tittle,R.id.setting_item_desc});
+        ListView lv_settings = findViewById(R.id.lv_settings);
+        lv_settings.setAdapter(simpleAdapter);
+        //dsahfashfa
     }
 }
