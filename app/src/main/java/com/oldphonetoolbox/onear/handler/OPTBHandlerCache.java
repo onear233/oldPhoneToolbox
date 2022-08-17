d7d5a3761754fd3189011ef76f070c0e361bac8f
microDreamSheep <atthemc313128460@gmail.com>
2022-08-17 19:47:00 +0800

    微调

--- app/src/main/java/com/oldphonetoolbox/onear/handler/OPTBHandlerCache.java
+++ app/src/main/java/com/oldphonetoolbox/onear/handler/OPTBHandlerCache.java
@@ -1,15 +1,24 @@
 package com.oldphonetoolbox.onear.handler;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.oldphonetoolbox.onear.handler.error.OPTBErrorHandler;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class OPTBHandlerCache {
     private static final Map<Byte,OPTBHandlerInterface> handlers_map = new HashMap<>();
+    private static final OPTBHandlerInterface ERROR = new OPTBErrorHandler();
     static{
         //注入handler数据
     }
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static OPTBHandlerInterface getHandler(byte id){
-        return handlers_map.get(id);
+        return Optional.ofNullable(handlers_map.get(id)).orElse(ERROR);
     }
     public static OPTBHandlerInterface addHandler(byte id,OPTBHandlerInterface handler){
         return handlers_map.put(id,handler);
--- app/src/main/java/com/oldphonetoolbox/onear/socket/SocketCoreController.java
+++ app/src/main/java/com/oldphonetoolbox/onear/socket/SocketCoreController.java
@@ -49,7 +49,7 @@ public class SocketCoreController {
             byte[] array = getMetaDataBytes(metaData);
             //获取携带数据长度
             ByteBuffer byteBuffer = ByteBuffer.allocate(array[0] << 8 | array[1]);
-            Optional.ofNullable(OPTBHandlerCache.getHandler(array[2])).orElse(new OPTBErrorHandler()).execute(getMetaDataBytes(byteBuffer),activity);
+            OPTBHandlerCache.getHandler(array[2]).execute(getMetaDataBytes(byteBuffer),activity);
         }
     }
     private byte[] getMetaDataBytes(ByteBuffer byteBuffer){
