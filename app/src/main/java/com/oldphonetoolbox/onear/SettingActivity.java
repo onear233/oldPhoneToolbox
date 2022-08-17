bf11fec47b43aa2120387aaa707c94645ed8d10b
onear <mifen2017@qq.com>
2022-08-17 20:29:58 +0800

    设置的界面布局完成

--- app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
+++ app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
@@ -9,11 +9,15 @@ import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SettingActivity extends AppCompatActivity {
     //数据源，tittleArray为标题，descriptionArray为描述
-    private final String[] titleArray = {getResources().getString(R.string.settings_color_title),getResources().getString(R.string.settings_st2_desc)};
-    private final String[] descriptionArray = {getResources().getString(R.string.settings_color_desc),getResources().getString(R.string.settings_st2_desc)};
+//    private final String[] titleArray = {getResources().getString(R.string.settings_color_title),getResources().getString(R.string.settings_st2_desc)};
+//    private final String[] descriptionArray = {getResources().getString(R.string.settings_color_desc),getResources().getString(R.string.settings_st2_desc)};
+    private final String[] titleArray = {"uioluiolu","getResources().getString(R.string.settings_st2_desc)"};
+    private final String[] descriptionArray = {"getResources().getString(R.string.settings_color_desc)","getResources().getString(R.string.settings_st2_desc)"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +26,14 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void showListview() {
-        List<HashMap<String, String>> list = new ArrayList<>();
+        List<Map<String, Object>> list = new ArrayList<>();
         for (int i = 0; i < titleArray.length; i++) {
-            HashMap <String,String> item = new HashMap<>();
-            item.put("tittle",titleArray[i]);
+            Map<String,Object> item = new HashMap<>();
+            item.put("title",titleArray[i]);
             item.put("desc",descriptionArray[i]);
             list.add(item);
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this,list,R.layout.simple_list_item,new String[]{"tittle","desc"},new int[]{R.id.setting_item_tittle,R.id.setting_item_desc});
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,list,R.layout.simple_list_item,new String[]{"title","desc"},new int[]{R.id.setting_item_tittle,R.id.setting_item_desc});
         ListView lv_settings = findViewById(R.id.lv_settings);
         lv_settings.setAdapter(simpleAdapter);
         //dsahfashfa
