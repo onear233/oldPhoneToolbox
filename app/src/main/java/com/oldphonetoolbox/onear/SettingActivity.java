5c05792bc1fbebe2cf354470ebb3fc2975fae948
onear <mifen2017@qq.com>
2022-08-18 17:06:50 +0800

    设置增加功能

--- app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
+++ app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -12,13 +14,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     //数据源，tittleArray为标题，descriptionArray为描述
 
     Constant constant = new Constant();
     private final String[] titleArray = {constant.settingsColorTitle,constant.settingsPasswordTitle};
     private final String[] descriptionArray = {constant.settingsColorDesc,constant.settingsPasswordDesc};
-
+    private List<Map<String, Object>> list;
+    private HashMap<String, Object> item;
 
 
     @Override
@@ -29,16 +32,25 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void showListview() {
-        List<Map<String, Object>> list = new ArrayList<>();
+        list = new ArrayList<>();
         for (int i = 0; i < titleArray.length; i++) {
-            Map<String,Object> item = new HashMap<>();
+            item = new HashMap<>();
             item.put("title",titleArray[i]);
             item.put("desc",descriptionArray[i]);
             list.add(item);
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this,list,R.layout.simple_list_item_tv_only,new String[]{"title","desc"},new int[]{R.id.setting_item_tittle,R.id.setting_item_desc});
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list,R.layout.simple_list_item_tv_only,new String[]{"title","desc"},new int[]{R.id.setting_item_tittle,R.id.setting_item_desc});
         ListView lv_settings = findViewById(R.id.lv_settings);
         lv_settings.setAdapter(simpleAdapter);
-        lv_settings.Listener
+        lv_settings.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            case 0:
+
+        }
     }
 }
---
+++ app/src/main/java/com/oldphonetoolbox/onear/TranslateActivity.java
@@ -0,0 +1,22 @@
+package com.oldphonetoolbox.onear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class TranslateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_translate);
+    }
+}
