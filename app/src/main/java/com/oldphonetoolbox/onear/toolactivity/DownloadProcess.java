a7e3f643c6f891b98276981fa321a88fbbe03d1d
onear <mifen2017@qq.com>
2022-08-20 15:00:11 +0800

    下载界面布局

--- app/src/main/java/com/oldphonetoolbox/onear/toolactivity/DownloadProcess.java
+++ app/src/main/java/com/oldphonetoolbox/onear/toolactivity/DownloadProcess.java
@@ -2,15 +2,29 @@ package com.oldphonetoolbox.onear.toolactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import com.oldphonetoolbox.onear.R;
 
 public class DownloadProcess extends AppCompatActivity {
 
+    private int downloadProcess;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_download_process);
+        ProgressBar downloadProgress = findViewById(R.id.pb_download);
+
+        downloadProgress.setProgress(downloadProcess);
     }
 }
