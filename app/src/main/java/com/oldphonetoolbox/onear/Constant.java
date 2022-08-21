f3b4970eec256d7e752f021037d6829fdbad1f6b
onear <mifen2017@qq.com>
2022-08-21 11:34:19 +0800

    设置功能增强

--- app/src/main/java/com/oldphonetoolbox/onear/Constant.java
+++ app/src/main/java/com/oldphonetoolbox/onear/Constant.java
@@ -5,6 +5,9 @@ public class Constant {
     final String settingsColorDesc = "你可以选择你喜欢的颜色。AMOLED屏幕设置背景为纯黑色可以达到省电的效果";
     final String settingsPasswordTitle = "设置密码";
     final String settingsPasswordDesc = "这可以使你的连接更加安全";
-
+    final String settingsIpAddressTitle = "设置电脑IP地址";
+    final String settingsIpAddressDesc = "设置电脑的IP地址，这是连接电脑所必需的";
+    final String helpAndAboutTitle = "帮助与支持";
+    final String helpAndAboutDesc = "在这里可以看到此应用的使用方法和关于开发者的信息";
 
 }
--- app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
+++ app/src/main/java/com/oldphonetoolbox/onear/SettingActivity.java
@@ -3,7 +3,7 @@ package com.oldphonetoolbox.onear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,13 +18,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SettingActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     //数据源，tittleArray为标题，descriptionArray为描述
 
     Constant constant = new Constant();
-    private final String[] titleArray = {constant.settingsColorTitle,constant.settingsPasswordTitle};
-    private final String[] descriptionArray = {constant.settingsColorDesc,constant.settingsPasswordDesc};
+    private final String[] settingTitleArray = {constant.settingsColorTitle,constant.settingsPasswordTitle,constant.settingsIpAddressTitle};
+    private final String[] settingDescriptionArray = {constant.settingsColorDesc,constant.settingsPasswordDesc,constant.settingsIpAddressDesc};
+    private final String[] helpTitleArray = {constant.helpAndAboutTitle};
+    private final String[] helpDescriptionArray = {constant.helpAndAboutDesc};
     private List<Map<String, Object>> list;
     private HashMap<String, Object> item;
     private EditText editText;
@@ -34,33 +37,71 @@ public class SettingActivity extends AppCompatActivity implements AdapterView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        showListview();
+        showListview(settingTitleArray,settingDescriptionArray,"title","desc",R.id.setting_item_tittle,R.id.setting_item_desc,R.id.lv_settings);
+        showListview(helpTitleArray,helpDescriptionArray,"helpTitle","helpDesc",R.id.setting_item_tittle,R.id.setting_item_desc,R.id.lv_help);
     }
 
-    private void showListview() {
+    private void showListview(String titleArray[],String descArray[],String title,String description,int idT,int idD,int listViewId) { //idT为条目布局
         list = new ArrayList<>();
         for (int i = 0; i < titleArray.length; i++) {
             item = new HashMap<>();
-            item.put("title",titleArray[i]);
-            item.put("desc",descriptionArray[i]);
+            item.put(title,titleArray[i]);
+            item.put(description,descArray[i]);
             list.add(item);
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list,R.layout.simple_list_item_tv_only,new String[]{"title","desc"},new int[]{R.id.setting_item_tittle,R.id.setting_item_desc});
-        ListView lv_settings = findViewById(R.id.lv_settings);
-        lv_settings.setAdapter(simpleAdapter);
-        lv_settings.setOnItemClickListener(this);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list,R.layout.simple_list_item_tv_only,new String[]{title,description},new int[]{idT,idD});
+        if (listViewId == R.id.lv_settings){
+            ListView settingListView = findViewById(listViewId);
+            settingListView.setAdapter(simpleAdapter);
+            settingListView.setOnItemClickListener(this);
+        }else if(listViewId == R.id.lv_help){
+            ListView helpListView = findViewById(listViewId);
+            helpListView.setAdapter(simpleAdapter);
+            helpListView.setOnItemClickListener(this);
+        }
+
     }
 
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        switch (position){
-            case 0:
-                break;
-            case 1:
-                showPasswordEnterDialog();//显示对话框的方法
-                break;
+
+        if (parent.getId() == R.id.lv_help){ //判断是哪个Listview
+            Intent intent = new Intent(this,HelpActivity.class);
+            startActivity(intent);
+        }else if(parent.getId() == R.id.lv_settings){
+            switch (position){
+                case 0:
+                    break;
+                case 1:
+                    showPasswordEnterDialog();//显示对话框的方法
+                    break;
+                case 2:
+                    showIpAddressEnterDialog();//显示输入电脑Ip地址的方法
+            }
+        }else{
+            return;
         }
+    }
+
+    private void showIpAddressEnterDialog() {
+        editText = new EditText(SettingActivity.this);
+        AlertDialog.Builder iPInputDialog = new AlertDialog.Builder(SettingActivity.this);
+        iPInputDialog.setTitle("请输入电脑的IP地址").setView(editText);
+        iPInputDialog.setPositiveButton("确定",(dialog, which) -> {
+            String ip = "^((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])$";
+            String input = editText.getText().toString();
+            boolean isLegal = Pattern.matches(ip,input);
+            if (isLegal){
+                saveStr("ipAddress",input);
+            }else{
+                Toast.makeText(SettingActivity.this,"请输入正确的ip地址",Toast.LENGTH_SHORT).show();
+            }
+        });
+        iPInputDialog.setNegativeButton("取消", (dialog, which) -> { return; });
+        AlertDialog dialog = iPInputDialog.create();
+        dialog.show();//展示对话框
+        getStr("ipAddress");
     }
 
     private void showPasswordEnterDialog() {
@@ -69,31 +110,30 @@ public class SettingActivity extends AppCompatActivity implements AdapterView.On
         inputDialog.setTitle("请输入密码").setView(editText);
         inputDialog.setPositiveButton("确定", (dialog, which) -> {
             if (editText.getText().toString().length() != 0){
-                savePassword(editText.getText().toString());
+                saveStr("pwd",editText.getText().toString());
             }else{
-                Toast.makeText(SettingActivity.this,"密码为空！",Toast.LENGTH_SHORT);
-                return;
+                Toast.makeText(SettingActivity.this,"密码为空！",Toast.LENGTH_SHORT).show();
             }
         });
         inputDialog.setNegativeButton("取消", (dialog, which) -> { return; });
         AlertDialog dialog = inputDialog.create();
         dialog.show();//展示对话框
-        getPassword();
+        getStr("pwd");
 
 
     }
 
-    private void getPassword() {
-        SharedPreferences getPWD = getSharedPreferences("password", MODE_PRIVATE);
-        String getPassword = getPWD.getString("pwd","");
+    private void getStr(String key) {
+        SharedPreferences getPWD = getSharedPreferences(key, MODE_PRIVATE);
+        String getPassword = getPWD.getString(key,"");
         editText.setText(getPassword);
     }
 
 
-    private void savePassword(String pwd) {
-        SharedPreferences password = getSharedPreferences("password", MODE_PRIVATE);
+    private void saveStr(String id,String pwd) {
+        SharedPreferences password = getSharedPreferences(id, MODE_PRIVATE);
         SharedPreferences.Editor editor = password.edit();
-        editor.putString("pwd",pwd);
+        editor.putString(id,pwd);
         editor.commit();
     }
 }
